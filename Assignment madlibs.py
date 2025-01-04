b89469d54389a613c585da3f009ffe6513e134c9
Shola2492 <gbengastephen2025@gmail.com>
2025-01-04 02:41:38 +0100

    Add files via upload

---
+++ Assignment madlibs.py
@@ -0,0 +1,26 @@
+# Madlibs game
+# a word game where you create a story
+# by filing in blanks with random words
+
+adjective1 = input("Enter an adjective (descriptive): ")
+noun1 = input("Enter a noun(person, place, things): ")
+adjective2 = input("Enter an adjective (descriptive): ")
+verb1 = input("Enter a verb ending with 'ing'")
+adjective3 = input("Enter an adjective (descriptive): ")
+
+
+
+
+
+print(f"Today i went to a {adjective1} zoo")
+print(f"In an exibit, i saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
+
+
+#Arithemetric operators
+
+friend = 0
+friend = friend + 1
+
+print(friend)
